8aaa492a42cc9c0fae70125bb269bd24ecbf6af8
huyinjie <huyinjie1998@gmail.com>
2019-09-11 00:16:06 +0800

    Update search.sql

--- SQL/search.sql
+++ SQL/search.sql
@@ -1,17 +1,14 @@
--- Populate member_info
+-- 查找社员所加的社团 无club_member_link_id
 SELECT mi.*,ci.* FROM member_info mi
     left join club_member_link cml on mi.member_id = cml.member_id
     left join club_info ci on ci.club_id = cml.club_id
-
--- Populate member_info
-SELECT * FROM member_info
-	left join club_info
-
--- Populate member_info
-a=SELECT member_id FROM member_info WHERE member_username = 'admin-hj'
-b=SELECT club_id FROM club_info WHERE club_name = '火箭'
-INSERT INTO club_management.club_member_link(club_id, member_id)
-VALUES (null, a,b)
-
-
+-- 查找社员所加的社团 有club_member_link_id
+SELECT mi.*,ci.*,cml.club_member_link_id FROM member_info mi
+    left join club_member_link cml on mi.member_id = cml.member_id
+    left join club_info ci on ci.club_id = cml.club_id
+-- 查找某社员所加的社团
+SELECT mi.*,ci.* FROM member_info mi
+    left join club_member_link cml on mi.member_id = cml.member_id
+    left join club_info ci on ci.club_id = cml.club_id
+	WHERE mi.member_id = '1'
 
